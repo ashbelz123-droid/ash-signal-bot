37467a50356faffc86dfe027c63d444b45c028f8
ashbelz123-droid <ashbelz123@gmail.com>
2026-03-01 17:39:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT;
+// VERY IMPORTANT
+const PORT = process.env.PORT || 10000;
 
+// Simple route
 app.get("/", (req, res) => {
     res.send("🔥 Ash Signal Bot is Live");
 });
 
+// Force bind to 0.0.0.0
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port", PORT);
 });
